0473158a5e34ece96892b32389fe4340994354d4
Andy McKay <amckay@mozilla.com>
2014-03-05 09:02:54 -0800

    fix up jenkins

--- jenkins.py
+++ jenkins.py
@@ -1,22 +1,29 @@
 import datetime
 import requests
 
-REPOS = ['solitude', 'marketplace', 'marketplace-api',
-         'marketplace-webpay', 'amo-master', 'solitude']
+REPOS = ['solitude', 'marketplace', 'marketplace-api', 'marketplace-assets',
+         'amo-assets', 'olympia', 'marketplace-webpay', 'amo-master',
+         'solitude']
 
-def get_date(repo, build):
+def get_data(repo, build):
     url = ('https://ci-addons.allizom.org/job/{0}/{1}/api/json'
            .format(repo, build))
     res = requests.get(url, headers={'Accept': 'application/json'})
-    return datetime.datetime.strptime(res.json()['id'], '%Y-%m-%d_%H-%M-%S')
+    return res.json()
 
 allow = 10800
 
 def get_jenkins(repos):
     reqs = []
     for key in repos:
-        last_success = get_date(key, 'lastStableBuild')
-        diff = datetime.datetime.now() - last_success
+        if get_data(key, 'lastSuccessfulBuild')['result'] == 'SUCCESS':
+            continue
+
+        last_failure = datetime.datetime.strptime(
+            get_data(key, 'lastStableBuild')['id'],
+            '%Y-%m-%d_%H-%M-%S')
+
+        diff = datetime.datetime.now() - last_failure
         if diff.total_seconds() > allow:
             reqs.append('{0} unstable for {1} days, {2} hours'.
                         format(key, diff.days, diff.seconds / 3600))
